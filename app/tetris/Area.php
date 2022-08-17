7c8b310f88867f760b37699f3b44d240a0e67436
Asahi <songbenxu@gmail.com>
2022-08-17 17:47:36 +0900

    テストの期待値が無駄に配列になってるので修正＋「マスの情報をオブジェクトで管理」するためのリファクタリングを実施

--- app/tetris/Area.php
+++ app/tetris/Area.php
@@ -6,41 +6,29 @@ class Area
 	readonly private array $rows;
 
 	public function __construct(array $rows = null) {
-		$this->rows = $rows ?? [
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-			["□□□□□□□□□□"],
-		];
+		$this->rows = $rows ?? array_fill(0, 20, $this->generateEmptyRow());
+	}
+
+	public function generateEmptyRow() : array
+	{
+		return array_fill(0, 10, new Square());
 	}
 
 	public function placeBlock(int $row, int $col) : self
 	{
 		$rows = $this->rows;
 		$row_index = count($rows) - $row - 1;
-		$rows[$row_index] = substr_replace($rows[$row_index], "■", $col*3, 3);
+		$rows[$row_index][$col] = new Square(true);
 		return new self($rows);
 	}
 
 	public function render() : array
 	{
-		return $this->rows;
+		return array_map(
+			fn($row) => implode('', array_map(
+				fn(Square $square) => $square->render(), $row)
+			),
+			$this->rows
+		);
 	}
 }
---
+++ app/tetris/Square.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tetris;
+
+class Square
+{
+	const RENDER_CHAR_BLOCK = "■";
+	const RENDER_CHAR_EMPTY = "□";
+
+	readonly private bool $hasBlock;
+
+	public function __construct(bool $hasBlock = false) {
+		$this->hasBlock = $hasBlock;
+	}
+
+	public function render() : string
+	{
+		return $this->hasBlock ? self::RENDER_CHAR_BLOCK : self::RENDER_CHAR_EMPTY;
+	}
+}
